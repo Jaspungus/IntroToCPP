38e09d2ed03285667884a48d220adcf304bec679
Jaspungus <158242467+Jaspungus@users.noreply.github.com>
2024-01-31 13:48:31 +1030

    Setting it up dawg

---
+++ Intro Project/Intro Project.cpp
@@ -0,0 +1,92 @@
+// Intro Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // Celsius to Fahrenheit)
+
+    //float degCelsius = 78.0f;
+    //float degFahrenheit = 0.0f; // Modify this variable below.
+
+
+    //// <Your work goes here>
+
+    //std::cin >> degCelsius;
+    //degFahrenheit = (degCelsius) * (9.0f / 5.0f) + 32.0f;
+
+    //std::cout << "Celsius to Fahrenheit)" << std::endl;
+    //std::cout << "Celsius:    " << degCelsius << std::endl;
+    //std::cout << "Fahrenheit: " << degFahrenheit << std::endl;
+
+
+    // Area of a Rectangle)
+
+    //float rectWidth = 0.0f;  // Modify this variable below.
+    //float rectHeight = 0.0f; // Modify this variable below.
+    //float rectArea = 0.0f;   // Modify this variable below.
+
+    //std::cout << "Input 2 numbers" << std::endl;
+    //std::cin >> rectWidth;
+    //std::cin >> rectHeight;
+    //rectArea = rectWidth * rectHeight;
+
+    //std::cout << "Area of a Rectangle)" << std::endl;
+    //std::cout << "H: " << rectHeight << " , W: " << rectWidth << std::endl;
+    //std::cout << "Area: " << rectArea << std::endl;
+    
+    // Average of Five)
+
+    //float a, b, c, d, e;    // Modify these variables below.
+    //float avg;              // Modify this variable below.
+    //avg = a = b = c = d = e = 0.0f; // Initialize all to zero.
+
+    //// <Your work goes here>
+
+
+    //std::cout << "Put in 5 numbers wah wah" << std::endl;
+    //   
+    //std::cin >> a >> b >> c >> d >> e;
+
+    //avg = (a + b + c + d + e) / 5.0f;
+
+    //std::cout << "Average of Five)" << std::endl;
+    //std::cout << a << "," << b << "," << c << "," << d << "," << e << std::endl;
+    //std::cout << "avg: " << avg << std::endl;
+
+    // Number Swap)
+
+    //int x = 13;
+    //int y = 24;
+
+    //// <Your work goes here>
+    //std::cout << "Put in 2 numbers wah wah" << std::endl;
+
+    //int z = x;
+    //x = y;
+    //y = x;
+
+
+    //std::cout << "Number Swap)" << std::endl;
+    //std::cout << "x is " << x << std::endl;
+    //std::cout << "y is " << y << std::endl;
+
+    // ...
+
+
+
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
